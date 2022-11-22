92199af018c3ba794ba3d8a1302524d808fef30b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-11-23 01:15:44 +0900

    change insert initial value

--- mysql/sql_init/02_initial-data.sql
+++ mysql/sql_init/02_initial-data.sql
@@ -5,13 +5,13 @@ INSERT INTO sample_schema.account (email, password, user_name) VALUES
 ;
 
 INSERT INTO sample_schema.users (email, password, first_name, last_name, birthdate, phone) VALUES
-('user1@local', 'user1pass', 'MR.user1', 'aaa', '2000/01/01', '888-888-888'),
-('user2@local', 'user2pass', 'MR.user2', 'bbb', '2000/01/01', '888-888-888'),
-('user3@local', 'user3pass', 'MR.user3', 'ccc', '2000/01/01', '888-888-888');
+('user1@local', 'user1pass', 'Amir', 'aaa', '2000/01/01', '888-888-888'),
+('user2@local', 'user2pass', 'Hiro', 'bbb', '2000/01/01', '888-888-888'),
+('user3@local', 'user3pass', 'Yuki', 'ccc', '2000/01/01', '888-888-888');
 
 INSERT INTO sample_schema.doctor_schedules (doctor_uuid, time) VALUES
 (1, '2000/01/01 10:00:00'),
 (1, '2000/01/01 10:15:00'),
 (1, '2000/01/01 10:30:00');
 
-INSERT INTO sample_schema.doctors (name) VALUES ('Prabh'), ('aaa'), ('bbb');
+INSERT INTO sample_schema.doctors (name) VALUES ('Prabh'), ('Amir'), ('Hiro'), ('Yuki');
